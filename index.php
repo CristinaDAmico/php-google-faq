7eea1b0730d3e5c4f3e0091f1f0aefaf721975c0
CristinaDAmico <crydamico94@gmail.com>
2021-05-19 15:45:53 +0200

    init

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+    <!-- ICONS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- STYLE -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <header class="d-flex">
+
+        <div class="header-left d-flex">
+            <a href="#">
+                <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="logo">
+            </a>
+            <a href="#">Privacy e termini</a>
+        </div>
+
+        <div class="d-flex">
+            <a href="#">
+                <i class="fas fa-ellipsis-v"></i>
+            </a>
+            <a href="#">
+                <img class="avatar" src="https://banner2.cleanpng.com/20180706/qsh/kisspng-computer-icons-avatar-user-profile-woman-woman-power-symbol-5b3fb75919cc26.7982583515309023611057.jpg" alt="">
+            </a>
+        </div>
+
+    </header>
+
+    <nav>
+        <ul class="d-flex">
+            <li>
+                <a href="#">Introduzione</a>
+            </li>
+            <li>
+                <a href="#">Norme sulla privacy</a>
+            </li>
+            <li>
+                <a href="#">Termini di servizio</a>
+            </li>
+            <li>
+                <a href="#">Tecnologie</a>
+            </li>
+            <li>
+                <a href="#">Domande frequenti</a>
+            </li>
+        </ul>
+    </nav>
+
+    <main class="container">
+        
+    </main>
+    
+</body>
+</html>
